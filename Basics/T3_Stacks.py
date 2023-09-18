0d3ee9898f0933f3ae8f6ebca4f1e47877e2320e
kahitoz <kshitiz.sinha.025@gmail.com>
2023-09-18 23:23:20 +0530

    Python module for implement stack

--- Basics/T3_Stacks.py
+++ Basics/T3_Stacks.py
@@ -8,3 +8,13 @@ for i in range(0, 11):
     stack.append(i)
 
 print(stack.pop())
+
+# using collection
+import collections
+s1 = collections.deque()
+s1.append(10)
+s1.append(20)
+s1.append(30)
+s1.append(40)
+print(s1)
+print("Popped Value = :", s1.pop())
