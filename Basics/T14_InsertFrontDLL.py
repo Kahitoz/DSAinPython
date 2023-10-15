2b3711c73fec6a9059808687f41d01f14c7f7285
kahitoz <kshitiz.sinha.025@gmail.com>
2023-10-15 19:44:54 +0530

    Insert node in front of Doubly Linked List

--- Basics/T14_InsertFrontDLL.py
+++ Basics/T14_InsertFrontDLL.py
@@ -8,17 +8,29 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-    def insert_front(self,data):
+    def insert_front(self, data):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
             self.tail = new_node
         else:
-            pointer = self.tail
-            while pointer.prev is not None:
-                pointer = pointer.prev
-            pointer.prev = new_node
-            new_node.next = pointer
-            self.tail = new_node
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
 
+
+    def print_forward(self):
+        if self.head is not None:
+            pointer = self.head
+            while pointer is not None:
+                print(pointer.data,end = " ")
+                pointer = pointer.next
+            print()
+        else:
+            print("No elements present in the list")
+
+DLL = DoublyLinkedList()
+for i in range(0,10):
+    DLL.insert_front(i+1)
+DLL.print_forward()
                 
